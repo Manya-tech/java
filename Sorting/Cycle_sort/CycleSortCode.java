7e2365534e28eb62b97be46ea0a0d3a5be5e5135
Manya-tech <mgmanya2311@gmail.com>
2023-08-26 07:47:58 +0530

    Cyclic Sort

--- Sorting/Cycle_sort/CycleSortCode.java
+++ Sorting/Cycle_sort/CycleSortCode.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class CycleSortCode {
     
     public static void main(String[] args) {
-        int[] arr = {3,2,4,5,1};
+        int[] arr = {3,1,3,4,2,5,6,7};
         cycleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
